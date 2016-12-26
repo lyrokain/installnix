451044fbd18fe146999a83dc91c1589555132962
lyrokain <lyrokain@k21.org>
2016-12-26 14:04:40 +0100

    initial commit

---
+++ go.sh
@@ -0,0 +1,11 @@
+loadkeys de
+echo ",,83,*"|sfdisk /dev/sda
+mkfs.ext4 -L nixos /dev/sda
+mount /dev/disk/by-label/nixos /mnt
+nixos-generate-config --root /mnt
+nix-env --install git
+git clone https://github.com/lyrokain/installnix /mnt/etc/nixos/
+
+# cp /etc/nixos/
+# nano /mnt/etc/nixos/configuration.nix
+time nixos-install
