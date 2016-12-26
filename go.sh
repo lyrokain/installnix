393d0eb45d2c8dd7224ebcafd0e9c9dcf5ea90f3
lyrokain <lyrokain@k21.org>
2016-12-26 14:47:41 +0100

    mal sehen

--- go.sh
+++ go.sh
@@ -2,10 +2,13 @@ loadkeys de
 echo ",,83,*"|sfdisk /dev/sda
 mkfs.ext4 -L nixos /dev/sda1
 mount /dev/disk/by-label/nixos /mnt
-nixos-generate-config --root /mnt
 nix-env --install git
 git clone https://github.com/lyrokain/installnix /mnt/etc/nixos/
-
+pause Press Enter to list config directory
+ls /mnt/etc/nixos/
+pause Press Enter to generate config.nix and hardware.nix
+nixos-generate-config --root /mnt
+echo run nixos-install to install the system
 # cp /etc/nixos/
 # nano /mnt/etc/nixos/configuration.nix
 time nixos-install
